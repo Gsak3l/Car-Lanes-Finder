020a9d81c3f5a40a605d4e16dc1d83ddcd2b8054
gsak3l <gsakel5@gmail.com>
2020-12-05 21:53:00 +0200

    combo image added to the code

--- lanes.py
+++ lanes.py
@@ -28,7 +28,7 @@ def display_lines(image, lines_):
         for line in lines_:  # printing all the lines
             x1, y1, x2, y2 = line.reshape(4)  # reshape into one array with one dimensional array with 4  elements
             # specifying the coordinates for where the lines have to be drawn
-            cv2.line(image_line, (x1, y1), (x2, y2), (255, 0, 0), 10)  # color and line thickness
+            cv2.line(image_line, (x1, y1), (x2, y2), (239, 255, 0), 10)  # color and line thickness, BRG NOT RGB
     return image_line
 
 
@@ -40,9 +40,10 @@ cropped_image = region_of_interest(canny)  # calling the function region_of_inte
 # precision in pixels, degree of precision, and threshold, empty array, length of a line in px, max distance in px
 # long story short this took me about 1 hour and a half to understand, and detects lines in the image
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
+line_image = display_lines(lane_image, lines)  # calling the function
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)  # 0.8 and 1 is the weight of the image
 
-cv2.imshow('result', line_image)  # render ing the image
+cv2.imshow('result', combo_image)  # render ing the image
 cv2.waitKey(0)  # without that, the image would disappear really fast, 0 = infinite, goes away when pressing a key
 
 # plt.imshow(canny)
